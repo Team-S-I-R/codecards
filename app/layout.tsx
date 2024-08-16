f4166246e7336060a17fade8a4e68c9a78a63e2c
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-08-16 14:03:42 -0400

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
